05a823a3f83d5546de9e7c6b95bb357ca4acde46
Tuan PM <tuanpm@live.com>
2015-01-20 08:32:00 +0700

    add case MQTT_MSG_TYPE_SUBSCRIBE for mqtt_get_id

--- mqtt-msg.c
+++ mqtt-msg.c
@@ -267,6 +267,7 @@ uint16_t mqtt_get_id(uint8_t* buffer, uint16_t length)
     case MQTT_MSG_TYPE_PUBCOMP:
     case MQTT_MSG_TYPE_SUBACK:
     case MQTT_MSG_TYPE_UNSUBACK:
+    case MQTT_MSG_TYPE_SUBSCRIBE:
     {
       // This requires the remaining length to be encoded in 1 byte,
       // which it should be.
